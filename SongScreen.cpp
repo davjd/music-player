fab1c900ae3483f38fc177c9b4c7711d16b5930b
davjd <davidbepic@gmail.com>
2017-05-14 02:41:15 -0400

    Still working on screen.

--- SongScreen.cpp
+++ SongScreen.cpp
@@ -1,11 +1,14 @@
 #include "SongScreen.h"
 #include "ui_SongScreen.h"
+#include "Player.h"
+#include <QDebug>
 
 SongScreen::SongScreen(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::SongScreen)
 {
     ui->setupUi(this);
+    qDebug() << "player: " << player;
 }
 
 SongScreen::~SongScreen()
