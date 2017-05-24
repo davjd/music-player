da12c84f256a9bcffdd31b0618b4a508fc4c24e9
davjd <davidbepic@gmail.com>
2017-05-24 13:14:25 -0400

    Directory scanner almost done.

--- main.cpp
+++ main.cpp
@@ -56,8 +56,8 @@ MusicPlayer* player;
 //}
 
 
-void iterate(QVector< QVector<Song*>* >* list){
-    QDir p = QDir(QString(getenv("HOME")) + "/Music");
+void iterate(QHash<QString, QVector<Song*>* >* list){
+    QDir p = QDir(QString(getenv("HOME")) + "/Music/");
     QDirIterator it(p.absolutePath(), QDir::Files, QDirIterator::Subdirectories);
 
     // most common audio files:
@@ -70,23 +70,60 @@ void iterate(QVector< QVector<Song*>* >* list){
     while(it.hasNext()){
         QFileInfo f(it.filePath());
 
-        if(f.isFile()){
+        if(f.isDir()){
+            qDebug() << "found folder: " << f.absoluteFilePath();
+            QString folderName = f.absoluteFilePath();
+
+            if(list->contains(folderName)){
+                qDebug() << "Playlist for this folder already exists.";
+            }else{
+                qDebug() << "Playlist for this folder doesn't exist."
+                         << "\nCreating new Playlist";
+                list->insert(folderName, new QVector<Song*>());
+            }
+
+//            if(list->at(list->size() - 1)->size() == 0){
+//                qDebug() << "Last created list is empty. So used old list, instead of creating new one.";
+//            }
+//            else{
+//                qDebug() << "Last list was not empty, so creating new list.";
+//                list->append(new QVector<Song*>());
+//            }
+        }
+        else if(f.isFile()){
             if(valids.contains(f.suffix().toLower())){
-                qDebug() << "base: " << f.baseName();
-                player->insert(new Song(QDir(f.filePath())));
+                qDebug() << "Adding " << f.baseName() << " to key " << f.absolutePath() << " of lists.";
+                list->value(f.absolutePath())->push_back(new Song(QDir(f.filePath())));
+//                qDebug() << "base: " << f.baseName();
+//                list->at(list->size() - 1)->push_back(new Song(QDir(f.filePath())));
+//                player->insert(new Song(QDir(f.filePath())));
             }
         }
+
+        if(f.isFile()){
+                    if(valids.contains(f.suffix().toLower())){
+                        QString key;
+                        if(list->contains(f.absolutePath())){
+                            list->value(f.)
+                        }
+                        qDebug() << "Adding " << f.baseName() << " to key " << f.absolutePath() << " of lists.";
+                        list->value(f.absolutePath())->push_back(new Song(QDir(f.filePath())));
+        //                qDebug() << "base: " << f.baseName();
+        //                list->at(list->size() - 1)->push_back(new Song(QDir(f.filePath())));
+        //                player->insert(new Song(QDir(f.filePath())));
+                    }
+                }
         it.next();
     }
 
      //there will still be one more file, because of hasNext().
-    QFileInfo f(it.filePath());
-    if(f.isFile()){
-        if(valids.contains(f.suffix().toLower())){
-            qDebug() << "base: " << f.baseName();
-            player->insert(new Song(QDir(f.filePath())));
-        }
-    }
+//    QFileInfo f(it.filePath());
+//    if(f.isFile()){
+//        if(valids.contains(f.suffix().toLower())){
+//            qDebug() << "base: " << f.baseName();
+//            player->insert(new Song(QDir(f.filePath())));
+//        }
+//    }
 }
 
 
@@ -98,11 +135,16 @@ int main(int argc, char *argv[])
     player = new MusicPlayer();
     player->setVolume(100);
 
-    QVector< QVector<Song*> >* list = new QVector< QVector<Song*> >();
+    QHash<QString, QVector<Song*>* >* list = new QHash<QString, QVector<Song*>*>();
+    iterate(list);
+
+//    QVector< QVector<Song*>* >* list = new QVector< QVector<Song*>* >();
+//    list->append(new QVector<Song*>());
+//    iterate(list);
 //    setList(list);
 
 
-    QDir p = QDir(QString(getenv("HOME")) + "/Music/trav");
+    QDir p = QDir(QString(getenv("HOME")) + "/Music/etc");
     QDirIterator it(p.absolutePath(), QDir::Files, QDirIterator::Subdirectories);
 
     // most common audio files:
