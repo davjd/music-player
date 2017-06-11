16bfac86301a1adff40115e03ae484422f81d06e
davjd <davidbepic@gmail.com>
2017-06-11 14:53:39 -0400

    Playlist serializer finished.

--- Playlist.cpp
+++ Playlist.cpp
@@ -24,18 +24,22 @@ QString Playlist::title(){
 }
 
 void Playlist::read(const QJsonObject &json){
-
+    title_ = json["title"];
+    QJsonArray songs = json["songs"].toArray();
+    foreach(QJsonObject data, songs){
+        Song* song = new Song();
+        song->read(data);
+        list_->push_back(song);
+    }
 }
 
 void Playlist::write(QJsonObject &json){
-      json["title"] = title_;
-
-      QJsonArray songs;
-      foreach(Song* song, list_){
-          QJsonObject data;
-          song->write(data);
-          songs.push_back(data);
-      }
-
-      json["songs"] = songs;
+    json["title"] = title_;
+    QJsonArray songs;
+    foreach(Song* song, list_){
+      QJsonObject data;
+      song->write(data);
+      songs.push_back(data);
+    }
+    json["songs"] = QJsonValue(songs);
 }
