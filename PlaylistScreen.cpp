d340ee5c745b64dc1e24c975a3d47c423ea8187e
davjd <davidbepic@gmail.com>
2017-06-11 23:40:47 -0400

    Fixing qgridlayout.

--- PlaylistScreen.cpp
+++ PlaylistScreen.cpp
@@ -13,15 +13,25 @@ PlaylistScreen::PlaylistScreen(QWidget *parent) :
     list_ = new QVector<Playlist*>();
     qDebug() << "Playlist screen init().";
 
-    ImageBlock* b = new ImageBlock();
-    b->setTitle("Song" + QString::number(1));
-    ui->grid->addWidget(b);
+//    ImageBlock* b = new ImageBlock();
+//    b->setTitle("Song" + QString::number(1));
+//    ui->grid->addWidget(b, 1, 1);
 
-//    for(int i = 1; i < 6; ++i){
-//        ImageBlock* b = new ImageBlock();
-//        b->setTitle("Song " + i);
-//        ui->grid->addWidget(b);
-//    }
+    int ctr = 0;
+    const int COL_NUM = 3;
+
+    for(int i = 1; i < 6; ++i){
+        int col = ctr % COL_NUM;
+        int row;
+
+        if(col != 0) row = ctr - col;
+        else row = (ctr / COL_NUM);
+
+        ImageBlock* b = new ImageBlock();
+        b->setTitle("Song" + QString::number(i));
+        ui->grid->addWidget(b, row, col);
+        ++ctr;
+    }
 
 
 
